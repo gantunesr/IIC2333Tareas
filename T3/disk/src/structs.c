e0020da3fbd149486c591091b99fca6244e44773
Cristian Cortes <cacortes1@uc.cl>
2017-10-14 21:41:03 -0300

    fix qsort

--- T3/disk/src/structs.c
+++ T3/disk/src/structs.c
@@ -95,7 +95,7 @@ void sstf(Disk* disk, Queue* queue){
 }
 
 void fun_scan(Disk* disk, Queue* queue){
-  qsort(queue->array, disk->size - 1, sizeof(int), compare);
+  qsort(queue->array, disk->size, sizeof(int), compare);
   int index = get_index(queue, disk->head);
   if(!(disk->size - 1 - index)){index++;}
   for(int i = index; i < disk->size; i++){
@@ -109,7 +109,7 @@ void fun_scan(Disk* disk, Queue* queue){
 }
 
 void clook(Disk* disk, Queue* queue){
-  qsort(queue->array, disk->size - 1, sizeof(int), compare);
+  qsort(queue->array, disk->size, sizeof(int), compare);
   int index = get_index(queue, disk->head);
   if(!(disk->size - 1 - index)){index++;}
   for(int i = index; i < disk->size; i++){
